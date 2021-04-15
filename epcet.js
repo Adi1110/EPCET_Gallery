673bba76b14cdac4e461538d66d8e05fcec24733
Aditya <ranjan.aditya03@gmail.com>
2021-04-15 13:09:38 +0530

    fourth commit

--- epcet.js
+++ epcet.js
@@ -5,3 +5,15 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none"
 }
+
+function myFunction(){
+	var email = document.forms["myForm"]["email"].value;
+	var psw = document.forms["myForm"]["psw"].value;
+	
+	if(email=="epcet" && psw=="adi123"){
+		window.location.href="photo_gallery";
+	}
+	else{
+		alert("Invalid email and password");
+	}
+}
